ddb6ee1c5358ffc6e9106327042ae0fbb0ada38e
Rishabh Purwar <rishabh15772@gmail.com>
2022-03-01 10:53:15 +0530

    Remove comments

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -45,18 +45,3 @@ export const GlobalProvider = ({ children }) => {
     </GlobalContext.Provider>
   );
 };
-
-// 1. Context  -- kind of data
-// 2. Provider -- doosre component tak data pahuchaane ke liye
-// 3. Consumer -- aur us data ko get karne ke liye
-// Context API ke andar upar ki ye teen cheeze aati hai, isme Consumer part thoda lengthy hota hai
-// that's why we use "useContext". Now it will look like this,
-// 1. Context
-// 2. Provider
-// 3. useContext() hook
-
-// The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on.
-// React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state.
-
-// value={{transactions: state.transactions, deleteTransaction,}}
-// whatever data or actions we want to access from other components we pass it through provider inside value argument.
